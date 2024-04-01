5dae6b10e42cae216520145b4840c748e07284a3
victrok <victrokwtf@gmail.com>
2024-03-31 21:44:59 -0500

    Add max line length to .editorconfig, update app.config.ts, and add esbuild to package.json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TestComponent } from './test/test.component';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [NgIf, RouterOutlet, TestComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'angular0';
 }
+//
